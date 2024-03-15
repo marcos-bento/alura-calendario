adf953aa7242738f8df829a60ca39bdcdcd9b2b6
Marcos Bento <marcos.x.marcos@hotmail.com>
2024-03-15 16:23:50 -0300

    Finalização da aula 03

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 import style from './Formulario.module.scss';
-import { obterId } from '../../util';
-import { useSetRecoilState } from 'recoil';
-import { IEvento } from '../../interfaces/IEvento';
-import { listaDeEventosState } from '../../state/atom';
+import useAdicionarEvento from '../../state/hooks/useAdicionarEvento';
 
 const Formulario: React.FC = () => {
-  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+  const adicionarEvento = useAdicionarEvento();
 
   const [descricao, setDescricao] = useState('')
   const [dataInicio, setDataInicio] = useState('')
@@ -21,19 +18,23 @@ const Formulario: React.FC = () => {
 
   const submeterForm = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    const evento = ({
-      id: obterId(),
-      descricao,
-      inicio: montarData(dataInicio, horaInicio),
-      fim: montarData(dataFim, horaFim),
-      completo: false
-    })
-    setListaDeEventos(listaAntiga => [...listaAntiga, evento]);
-    setDescricao('')
-    setDataInicio('')
-    setHoraInicio('')
-    setDataFim('')
-    setHoraFim('')
+    try {
+      const evento = ({
+        descricao,
+        inicio: montarData(dataInicio, horaInicio),
+        fim: montarData(dataFim, horaFim),
+        completo: false
+      })
+      adicionarEvento(evento);
+      setDescricao('')
+      setDataInicio('')
+      setHoraInicio('')
+      setDataFim('')
+      setHoraFim('')
+    } catch (error) {
+      alert(error);
+    }
+
   }
   return (<form className={style.Formulario} onSubmit={submeterForm}>
     <h3 className={style.titulo}>Novo evento</h3>
---
+++ src/state/hooks/useDeletarEvento.ts
@@ -0,0 +1,12 @@
+import { useSetRecoilState } from "recoil";
+import { IEvento } from "../../interfaces/IEvento";
+import { listaDeEventosState } from "../atom";
+
+const useDeletarEvento = () =>{
+    const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+    return (evento: IEvento) => {
+        setListaDeEventos(listaAntiga => [...listaAntiga.filter(evt => evt.id !== evento.id)]);
+    }
+}
+
+export default useDeletarEvento;
